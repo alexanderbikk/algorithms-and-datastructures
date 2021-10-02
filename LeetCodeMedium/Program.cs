f59db0128c122ea163c818db56ccc529d316ff53
Alexander <alexander_bikk@outlook.com>
2021-10-02 18:54:30 +0300

    Add project files.

---
+++ LeetCodeMedium/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCodeMedium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
