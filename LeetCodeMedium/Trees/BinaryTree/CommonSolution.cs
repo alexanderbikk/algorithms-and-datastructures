e0c9c4889c4df80caf625d7ddefaf7e670660e59
Alexander <alexander_bikk@outlook.com>
2021-10-14 15:37:39 +0300

    iterative symmetric tree aded with quweu and stack

--- LeetCodeMedium/Trees/BinaryTree/CommonSolution.cs
+++ LeetCodeMedium/Trees/BinaryTree/CommonSolution.cs
@@ -1,4 +1,6 @@
 ﻿using AlgorithmsAndDataStructures.Trees.BST;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace LeetCodeMedium.Trees.BinaryTree
 {
@@ -11,6 +13,11 @@ namespace LeetCodeMedium.Trees.BinaryTree
         /// <returns></returns>
         public bool IsSymetricTree(Node<int> root)
         {
+            if (root == null)
+            {
+                return false;
+            }
+
             return IsMirror(root.LeftChild, root.RightChild);
         }
 
@@ -30,6 +37,99 @@ namespace LeetCodeMedium.Trees.BinaryTree
             {
                 return IsMirror(rootL.LeftChild, rootR.RightChild) && IsMirror(rootL.RightChild, rootR.LeftChild);
             }
+        }
+
+        /// <summary>
+        /// Iterative version using queue
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public bool IsSymetricTreeQueue(Node<int> root)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            var queue = new Queue<Node<int>>();
+
+            queue.Enqueue(root.LeftChild);
+            queue.Enqueue(root.RightChild);
+            
+
+            while (queue.Count != 0)
+            {
+                var rootL = queue.Dequeue();
+                var rootR = queue.Dequeue();
+
+                if (rootL == null && rootR == null)
+                {                   
+                    continue;                    
+                }
+
+                var isRootEquals = rootL?.Value == rootR?.Value;
+                if (!isRootEquals)
+                {
+                    return false;
+                }
+                else
+                {
+                    queue.Enqueue(rootL.LeftChild);
+                    queue.Enqueue(rootR.RightChild);
+
+                    queue.Enqueue(rootL.RightChild);
+                    queue.Enqueue(rootR.LeftChild);
+                }
+            }
+
+            return true;
+        }
+
+
+        /// <summary>
+        /// Iterative using stack
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public bool IsSymetricTreeStack(Node<int> root)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            var stack = new Stack<Node<int>>();
+
+           
+            stack.Push(root.RightChild);
+            stack.Push(root.LeftChild);
+
+            while (stack.Count != 0)
+            {
+                var rootL = stack.Pop();
+                var rootR = stack.Pop();
+
+                if (rootL == null && rootR == null)
+                {
+                    continue;
+                }
+
+                bool isRootEqual = rootL?.Value == rootR?.Value;
+                if (!isRootEqual)
+                {
+                    return false;
+                }
+                else
+                {                    
+                    stack.Push(rootR.LeftChild);
+                    stack.Push(rootL.RightChild);
+
+                    stack.Push(rootR.RightChild);
+                    stack.Push(rootL.LeftChild);                    
+                }
+            }
+
+            return true;
         }
     }
 }
