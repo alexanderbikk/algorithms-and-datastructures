cdadeab4a3c20174e5ef8e27b96c97d6d2cac128
Alexander <alexander_bikk@outlook.com>
2022-02-08 23:27:13 +0200

    MinMeetingRooms for lyft added

--- LyftLeetCode/Heaps/Solution.cs
+++ LyftLeetCode/Heaps/Solution.cs
@@ -1,61 +1,136 @@
-﻿using System.Collections;
-
-namespace LyftLeetCode.Heaps;
+﻿namespace LyftLeetCode.Heaps;
 
 
 public class Solution
 {
-    public class TaxiTimeCostsComparer : IComparer<int[]>
+    public TaxiSchedulingSolution TaxiScheduling { get; init; }
+
+    public MeetingRoomsSolution MeetingRoomsAdjusted { get; init; }
+
+    public Solution()
     {
-        /// Ideally we shouldn't care about sorting when different tripTimes have the same tripCosts. Since we don't care wich car do the trip when the tripCosts the same
-        /// So only tripCosts should be sorted
-        public int Compare(int[]? x, int[]? y)
-        {
-            return x![1] - y![1];
-        }
+        TaxiScheduling = new TaxiSchedulingSolution();
+        MeetingRoomsAdjusted = new MeetingRoomsSolution();
     }
 
-    /// <summary>
-    /// https://leetcode.com/discuss/interview-question/1189312/Lyft-Taxi-Scheduling
-    /// Ideally we shouldn't care about sorting when different tripTimes have the same tripCosts. Since we don't care wich car do the trip when the tripCosts the same    
-    /// It was sorted in leetcode example.
-    /// </summary>
-    /// <returns></returns>
-    public int TaxiScheduling(int n, int[] cabTravelTime)
-    {      
-        var minHeap = new PriorityQueue<int[], int[]>(cabTravelTime.Length, new TaxiTimeCostsComparer());
-
-        //put init cost in min heap. So the lowest time cost taxi will be on top of the heap.
-        //e.g [3, 1, 4] -> [{1, 1}; {3, 3}; {4, 4};]
-        for (var i = 0; i < cabTravelTime.Length; i++)
+    public class TaxiSchedulingSolution
+    {
+        public class TaxiTimeCostsComparer : IComparer<int[]>
         {
-            // tripInfo -> {tripCost, totalTripCost}. initially it will be the same values
-            var tripInfo = new int[] {cabTravelTime[i], cabTravelTime[i]};
-            //since we can't get TPriority when Dequeue .net PriorityQueue we should store info in TElement as well.
-            minHeap.Enqueue(tripInfo, tripInfo);
+            /// Ideally we shouldn't care about sorting when different tripTimes have the same tripCosts. Since we don't care wich car do the trip when the tripCosts the same
+            /// So only tripCosts should be sorted
+            public int Compare(int[]? x, int[]? y)
+            {
+                return x![1] - y![1];
+            }
         }
 
-        var minTime = 0;
-        // until we don't make all trips
-        while (n != 0)
+        /// <summary>
+        /// https://leetcode.com/discuss/interview-question/1189312/Lyft-Taxi-Scheduling
+        /// Ideally we shouldn't care about sorting when different tripTimes have the same tripCosts. Since we don't care wich car do the trip when the tripCosts the same    
+        /// It was sorted in leetcode example.
+        /// </summary>
+        /// <returns></returns>
+        public int TaxiScheduling(int n, int[] cabTravelTime)
         {
-            //get trip info
-            var tripInfo = minHeap.Dequeue();
-            var tripCost = tripInfo[0];
-            var totalTripCost = tripInfo[1];
+            var minHeap = new PriorityQueue<int[], int[]>(cabTravelTime.Length, new TaxiTimeCostsComparer());
+
+            //put init cost in min heap. So the lowest time cost taxi will be on top of the heap.
+            //e.g [3, 1, 4] -> [{1, 1}; {3, 3}; {4, 4};]
+            for (var i = 0; i < cabTravelTime.Length; i++)
+            {
+                // tripInfo -> {tripCost, totalTripCost}. initially it will be the same values
+                var tripInfo = new int[] { cabTravelTime[i], cabTravelTime[i] };
+                //since we can't get TPriority when Dequeue .net PriorityQueue we should store info in TElement as well.
+                minHeap.Enqueue(tripInfo, tripInfo);
+            }
+
+            var minTime = 0;
+            // until we don't make all trips
+            while (n != 0)
+            {
+                //get trip info
+                var tripInfo = minHeap.Dequeue();
+                var tripCost = tripInfo[0];
+                var totalTripCost = tripInfo[1];
+
+                minTime = totalTripCost;
+
+                totalTripCost += tripCost;
+                tripInfo[1] = totalTripCost;
+
+                //return tripInfo bavk with updated totalTripCost
+                // so we know the next trip will be again the lowest cost since we always compare by totalTripCost(see minheap comparer)
+                minHeap.Enqueue(tripInfo, tripInfo);
+                n--;
+            }
+
+            return minTime;
+        }
+
+    }
+
+    public class MeetingRoomsSolution
+    {
+
+        public class RoomComparer : IComparer<int[]>
+        {
+            public int Compare(int[]? x, int[]? y)
+            {
+                return x![1] == y![1] ? x[0] - y[0] : x[1] - y[1];
+            }
+        }
+
+        public int[][] MinMeetingRooms(int[][] intervals)
+        {
+            int[][] resultRooms = new int[intervals.Length][];
+
+            var comparer = Comparer<int>.Default;
+            Array.Sort(intervals, (x, y) =>  comparer.Compare(x[0], y[0]));
+
+            var meetings = new PriorityQueue<int[], int[]>(intervals.Length, new RoomComparer());
+
+            for (var i = 0; i < intervals.Length; i++)
+            {
+                var roomNumber = ScheduleMeetingInAvaliableRoom(meetings, (intervals[i][0], intervals[i][1]));
+                UpdateResultRooms(resultRooms, i, roomNumber);
+            }
+
+            return resultRooms;
+        }
+
+        private int ScheduleMeetingInAvaliableRoom(PriorityQueue<int[], int[]> meetings, (int startTime, int endTime) interval)
+        {
+            var roomNumber = 0;
+            if (meetings.Count == 0)
+            {
+                var room = new int[] { roomNumber, interval.endTime };
+                meetings.Enqueue(room, room);
+                return roomNumber;
+            }
+
+            var roomInfo = meetings.Peek();
+            roomNumber = roomInfo[0];
+            var lastAvaliableRoomTime = roomInfo[1];          
+
+            if (lastAvaliableRoomTime <= interval.startTime)
+            {
+                meetings.Dequeue();              
+            }
+            else
+            {
+                roomNumber = meetings.Count;
+            }
             
-            minTime = totalTripCost;
 
-            totalTripCost += tripCost;
-            tripInfo[1] = totalTripCost;
-
-            //return tripInfo bavk with updated totalTripCost
-            // so we know the next trip will be again the lowest cost since we always compare by totalTripCost(see minheap comparer)
-            minHeap.Enqueue(tripInfo, tripInfo);
-            n--;
+            meetings.Enqueue(new int[] { roomNumber, interval.endTime }, new int[] { roomNumber, interval.endTime });
+            return roomNumber;
         }
 
-        return minTime;
+        private void UpdateResultRooms(int[][] rooms, int meetingIndex, int roomNumber)
+        {
+            rooms[meetingIndex] = new int[] { meetingIndex, roomNumber };
+        }
     }
 }
 
--- LyftLeetCode/Program.cs
+++ LyftLeetCode/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+
 Console.WriteLine("Heaps");
 Heaps();
 Console.WriteLine();
@@ -9,9 +11,55 @@ static void Heaps()
     var solution = new LyftLeetCode.Heaps.Solution();
     //var n = 3;
     //var cabTravelTime = new int[] { 3, 4, 8};
-
+    Console.WriteLine("Taxi scheduling");
     var n = 10;
     var cabTravelTime = new int[] { 1, 3, 8, 3, 5 };
-    var minTime = solution.TaxiScheduling(n, cabTravelTime);
+    var minTime = solution.TaxiScheduling.TaxiScheduling(n, cabTravelTime);
+    
     Console.WriteLine(minTime);
+    Console.WriteLine();
+
+    Console.WriteLine("Meeting rooms scheduling");
+    //var intervals = new int[3][]
+    //{
+    //    new int[] { 0, 30 },
+    //    new int[] { 5, 10 },
+    //    new int[] { 15, 25 }
+
+    //};
+
+    //var intervals = new int[2][]
+    //{
+    //    new int[] { 7, 10 },
+    //    new int[] { 2, 4 }        
+
+    //};
+
+   // var intervals = new int[5][]
+   //{
+   //     new int[] { 4, 8 },
+   //     new int[] { 6, 8 },
+   //     new int[] { 9, 16 },
+   //     new int[] { 15, 16 },
+   //     new int[] { 17, 20 }
+   //};
+
+    var intervals = new int[7][]
+   {
+        new int[] { 2, 4 },
+        new int[] { 4, 5 },
+        new int[] { 4, 6 },
+        new int[] { 5, 6 },
+        new int[] { 7, 10 },
+        new int[] { 8, 9 },
+        new int[] { 8, 10 },
+   };
+    var rooms = solution.MeetingRoomsAdjusted.MinMeetingRooms(intervals);
+
+    for (int i = 0; i < rooms.Length; i++)
+    {
+        Console.WriteLine($"{rooms[i][0]} {rooms[i][1]}");
+    }
+    
+    Console.WriteLine();
 }
